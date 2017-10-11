5b7b1ec65de03f1ccbf27361d2bcc73fbce42861
Marina Barbosa <marina.veiga.barbosa@gmail.com>
2017-10-11 21:25:18 +0200

    [1] Solve problem
    - Deserialize

--- w1/src/w1/Database.java
+++ w1/src/w1/Database.java
@@ -4,11 +4,55 @@
  * and open the template in the editor.
  */
 package w1;
-
 /**
  *
- * @author marin
+ * @author marina
  */
-public class Database {
-    
+
+interface IConnection { 
+    char get(int index); 
+    void set(int index, char c); 
+    int length(); 
 }
+
+class Database { 
+    private char[] tab = new char[100]; 
+    /* ... */ 
+
+  
+    private Database(){};
+    
+    
+  public static IConnection getConnection() { 
+     return Connection.getInstance();
+  } 
+  private static class Connection implements IConnection { 
+    private Database db; 
+    /* ... */ 
+       
+    private static int index = 0;
+    private static Connection[] connection;
+
+    private Connection (Database db) {
+        this.db = db;
+    }
+    public static IConnection getInstance(){
+      /* ... */ 
+        if (connection == null) {
+            Database c = new Database();
+            connection = new Connection[]{new Connection(c),new Connection(c),new Connection(c)};
+        }
+     index = (index+1) % connection.length;
+     return connection[index]; 
+    } 
+    public char get(int index) { 
+      return db.tab[index]; 
+    } 
+    public void set(int index, char c) {
+      db.tab[index] = c; 
+    }
+    public int length() { 
+      return db.tab.length; 
+    } 
+  } 
+} 
--- w1/src/w1/W1.java
+++ w1/src/w1/W1.java
@@ -7,15 +7,25 @@ package w1;
 
 /**
  *
- * @author marin
+ * @author marina 
  */
-public class W1 {
+public class W1 { //Teste Class
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        IConnection s1 = Database.getConnection();
+        IConnection s2 = Database.getConnection();
+        IConnection s3 = Database.getConnection();
+        IConnection s4 = Database.getConnection();
+        
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
     }
     
 }
